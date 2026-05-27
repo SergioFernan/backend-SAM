e1f4f621ceb45153211ea3540baa99f29bea9566
sergio ivan fernandez galeano <sercolgi1@gmail.com>
2026-05-26 19:24:54 -0500

    agregar funcionalidad get a user probada

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,18 +1,29 @@
-import { insertUser } from "../services/product.service.js";
+import { UserModel } from "../models/User.model.js";
+import { dbGetUsers, dbCreateUser } from "../services/user.service.js";
 
 // function que se llama a user.routes.js para ejecutarse
-function getUsers(req, res) {
-    res.json({
-        msj: `Listar usuarios`
-    });
+async function getUsers(req, res) {
+    try {
+        const data = await dbGetUsers();
+        res.status(200).json({
+            msj: `obtener usuarios`,
+            data: data
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            msj: `error al obtener usuarios`
+        });
+    }
 }
 
 async function postUsers(req, res) {
 
     try {
         const inputData = req.body; // recibe el body
-        const data = await insertUser(inputData) // llama a la funcion insertUser de product.service.js para crear usuario
+        const data = await dbCreateUser(inputData) // llama a la funcion insertUser de product.service.js para crear usuario
         res.status(201).json({
+            msj: `usuario creado`,
             data: data
         });
 
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,7 +1,11 @@
 import { UserModel } from "../models/User.model.js"; // importo el modelo de la base de datos
 
-const insertUser = async (newUser) => {
+const dbCreateUser = async (newUser) => {
     return await UserModel.create(newUser); // creo usuario
 }
 
-export { insertUser };
+const dbGetUsers = async () => {
+    return await UserModel.find(); // busco usuarios
+}
+
+export { dbCreateUser, dbGetUsers };
