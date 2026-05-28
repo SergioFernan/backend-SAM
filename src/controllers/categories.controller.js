3ab4b0a7444d0478479c8593cd1ad92343809d9c
alejandrof <aforero979@gmail.com>
2026-05-27 21:34:42 -0500

    [+] Produc.model: Define name como 'unique'. [+]produc.controller

--- src/controllers/categories.controller.js
+++ src/controllers/categories.controller.js
@@ -43,7 +43,17 @@ const postCategory = async (req, res) => {   //La función tiene que ser async
 
     } catch (error) {
 
-        console.error(error);  //Mensaje en la consola para el desarrollador 
+        
+        
+        console.error( error.code );  //Mensaje en la consola para el desarrollador 
+
+        //Validamos si la propiedad tiene un valor único
+        if( error.code === 11000 ) {
+            return res.json({
+                msg: 'Error: Esa categoria ya existe.'
+            });
+            
+        };
 
         // Respondemos al ususario enciando un mensaje humano 
         res.status(500).json({
@@ -64,108 +74,120 @@ const getCategoryById = async (req, res) => {
         // La validación defensiva va dentro del TRY ( condicionamos previo a que ocurrea el error ) 
 
         if (!mongoose.Types.objectiD.isValid(id)) {      // --> En este campo se importa mongoose
-            
+
             return res.status(400).json({
                 msg: 'Error Id: No se encontró la categoría.'
-                });
-            };
-
-            const data = await dbGetCategoryById(id);
-
-            res.status(200).json({
-                msg: 'obtine un producto por ID',
-                data: data
             });
+        };
 
-        } catch (error) {
+        const data = await dbGetCategoryById(id);
 
-            console.error(error);
+        if( ! data ) { 
+            return res.json ({
+                msg: 'Error Id: El Id no existe'
+            });
+        };
 
-            /// Si la exepción va dentro del CATCH es ( esta opción genera más control sobre el error )
+        res.status(200).json({
+            msg: 'obtine un producto por ID',
+            data: data
+        });
 
-            res.status(500).json({
-                msg: 'ERROR: No se encontró el Id del producto'
+    } catch (error) {
+
+        console.error(error);
+
+        /// Si la exepción va dentro del CATCH es ( esta opción genera más control sobre el error )
+
+        res.status(500).json({
+            msg: 'ERROR: No se encontró el Id del producto'
+        });
+
+    };
+};
+
+
+
+
+const patchCategory = async (req, res) => {
+
+    try {
+
+        const id = req.params.id;
+        const inputData = req.body;
+
+        const data = await dbUpDateCategory(id, inputData)
+
+
+        res.status(200).json({
+            msj: 'Update category',
+            data: data
+        });
+
+
+    } catch (error) {
+        console.error(error);
+
+        // Validación Excespción: Manjear cuanndo ocurre el error
+
+        if (error.name === 'CastError') {
+
+            return res.status(400).json({
+                msg: 'Error Id: No se epudo actualizar'
+            });
+        };
+
+        res.status(500).json({
+            msg: 'No se pudo actualizar la información'
+        })
+
+
+
+    }
+};
+
+
+
+
+const deleteCategory = async (req, res) => {
+
+    try {
+
+        const id = req.params.id;
+
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+
+            return res.status(400).json({
+                msg: 'Error Id: No se pudo eliminar la categoria'
             });
 
         };
-    };
 
+        const data = await dbDeleteCategory(id);
 
-
-
-    const patchCategory = async (req, res) => {
-
-        try {
-
-            const id = req.params.id;
-            const inputData = req.body;
-
-            const data = await dbUpDateCategory(id, inputData)
-
-
-            res.status(200).json({
-                msj: 'Update category',
-                data: data
-            });
-
-
-        } catch (error) {
-            console.error(error);
-
-            // Validación Excespción: Manjear cuanndo ocurre el error
-
-            if (error.name === 'CastError') {
-
-                return res.status(400).json({
-                    msg: 'Error Id: No se epudo actualizar'
-                });
-            };
-
-            res.status(500).json({
-                msg: 'No se pudo actualizar la información'
-            })
-
-
-
-        }
-    };
-
-
-
-
-    const deleteCategory = async (req, res) => {
-
-        try {
-
-            const id = req.params.id;
-
-            if (!mongoose.Types.ObjectId.isValid(id)) {
-
-                return res.status(400).json({
-                    msg: 'Error Id: No se pudo eliminar la categoria'
-                })
-
-            }
-
-            const data = await dbDeleteCategory(id);
-
-            res.status(200).json({
-                msj: 'Delete category',
-                // data: data,
-                id: id
-            });
-
-        } catch (error) {
-
-            console.error(error);
-
-            res.status(500).json({
-                msg: 'No se pudo borrar la categoria'
+        if( ! data ) { 
+            return res.json ({
+                msg: 'No se puede eliminar una categria que no ha sido registrada'
             });
         };
+
+        res.status(200).json({
+            msj: 'Delete category',
+            // data: data,
+            id: id
+        });
+
+    } catch (error) {
+
+        console.error(error);
+
+        res.status(500).json({
+            msg: 'No se pudo borrar la categoria'
+        });
     };
+};
 
 
 
 
-    export { getCategory, postCategory, patchCategory, deleteCategory, getCategoryById };   
+export { getCategory, postCategory, patchCategory, deleteCategory, getCategoryById };   
